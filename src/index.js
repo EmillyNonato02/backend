69f0bf7a18dd86dfe8191f966dceb1573e9f36d8
Emilly Nayara Nonato Vieira <enonatovieira@gmail.com>
2025-10-20 19:49:41 -0300

    aaaaaaaaaaa

--- src/index.js
+++ src/index.js
@@ -1,4 +1,4 @@
-import express from "express"
+import express, { response } from "express"
 import cors from "cors"
 
 import { persons } from "./persons.js"
@@ -7,10 +7,20 @@ const app = express()
 const port = 3333
 
 app.use(cors())
-
+app.use(express.json())
 
 app.get("/", (request, response) =>{
     response.json(persons)
+})
+
+app.post("/cadastrar", (request, response) => {
+    //desestruturação
+    const { user } = request.body
+    console.log(user)
+
+    //cadastro no banco de dados
+
+    response.status(201).json({ message : "Usuário cadastrado com sucesso!" })
 })
 
 app.listen(port, () => {
